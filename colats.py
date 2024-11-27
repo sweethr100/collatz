3367614079d1a5996dd4e432bf94c7c8ac4fcfd9
sweethr100 <sweethr100@naver.com>
2024-11-27 22:25:39 +0900

    I made

---
+++ colats.py
@@ -0,0 +1,14 @@
+def colats(n):
+    x = n
+    while x != 1:
+        if x%2 == 0:
+            x= x/2
+        else:
+            x = 3*x + 1
+    return n
+
+if __name__ == "__main__":
+    n = 1
+    while True:
+        print(colats(n))
+        n+=1
